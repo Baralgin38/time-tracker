ca96b6829381d143e3976bab290cb00641afd565
Ilya Emelyanov <baralgin38@yandex.ru>
2022-08-06 18:55:43 +0300

    Describes the logic of working with tasks

--- server/routes/task.routes.js
+++ server/routes/task.routes.js
@@ -1,16 +1,67 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-// const Task = require("../models/Task");
+const Task = require("../models/Task");
+const auth = require("../middleware/auth.middleware");
 
-router.get("/", (req, res) => {
-  try {
-    // const tasks = await Task.find();
-    res.status(200).send("Задачи");
-  } catch (error) {
-    res.status(500).json({
-      message: "На сервере произошла ошибка, попробуйте позже.",
-    });
-  }
-});
+router
+  .route("/:projectId")
+  .post(auth, async (req, res) => {
+    try {
+      const { projectId } = req.params;
+
+      const newTask = await Task.create({
+        ...req.body,
+        projectId: projectId,
+      });
+
+      res.status(201).send(newTask);
+    } catch (error) {
+      res.status(500).json({
+        message: "На сервере произошла ошибка, попробуйте позже.",
+      });
+    }
+  })
+  .get(auth, async (req, res) => {
+    try {
+      const { projectId } = req.params;
+
+      const tasks = await Task.find({ projectId });
+
+      res.status(200).send(tasks);
+    } catch (error) {
+      res.status(500).json({
+        message: "На сервере произошла ошибка, попробуйте позже.",
+      });
+    }
+  });
+
+router
+  .route("/:taskId")
+  .patch(auth, async (req, res) => {
+    try {
+      const { taskId } = req.params;
+
+      const updatedTask = await Task.findByIdAndUpdate(taskId, req.body, {
+        new: true,
+      });
+      res.status(200).send(updatedTask);
+    } catch (error) {
+      res.status(500).json({
+        message: "На сервере произошла ошибка, попробуйте позже.",
+      });
+    }
+  })
+  .delete(auth, async (req, res) => {
+    try {
+      const { taskId } = req.params;
+      const removedTask = await Task.findById(taskId);
+      await removedTask.remove();
+      return res.send(null);
+    } catch (error) {
+      res.status(500).json({
+        message: "На сервере произошла ошибка, попробуйте позже.",
+      });
+    }
+  });
 
 module.exports = router;
