b7ed03c1b692643894a97ba13ee575b048bf35a7
Ilya Emelyanov <baralgin38@yandex.ru>
2022-08-06 18:55:02 +0300

    Describes the logic of working with projects

--- server/routes/project.routes.js
+++ server/routes/project.routes.js
@@ -1,16 +1,62 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-// const Project = require("../models/Project");
+const Project = require("../models/Project");
+const auth = require("../middleware/auth.middleware");
 
-router.get("/", (req, res) => {
-  try {
-    // const projects = await Project.find();
-    res.status(200).send("Проекты");
-  } catch (error) {
-    res.status(500).json({
-      message: "На сервере произошла ошибка, попробуйте позже.",
-    });
-  }
-});
+router
+  .route("/")
+  .get(auth, async (req, res) => {
+    try {
+      const projects = await Project.find({ creator: req.user._id });
+      res.status(200).send(projects);
+    } catch (error) {
+      res.status(500).json({
+        message: "На сервере произошла ошибка, попробуйте позже.",
+      });
+    }
+  })
+  .post(auth, async (req, res) => {
+    try {
+      const newProject = await Project.create({
+        ...req.body,
+        creator: req.user._id,
+      });
+      res.status(200).send(newProject);
+    } catch (error) {
+      res.status(500).json({
+        message: "На сервере произошла ошибка, попробуйте позже.",
+      });
+    }
+  });
+
+router
+  .route("/:projectId")
+  .patch(auth, async (req, res) => {
+    try {
+      const { projectId } = req.params;
+      const updatedProject = await Project.findByIdAndUpdate(
+        projectId,
+        req.body,
+        { new: true }
+      );
+      res.status(200).send(updatedProject);
+    } catch (error) {
+      res.status(500).json({
+        message: "На сервере произошла ошибка, попробуйте позже.",
+      });
+    }
+  })
+  .delete(auth, async (req, res) => {
+    try {
+      const { projectId } = req.params;
+      const removedProject = await Project.findById(projectId);
+      await removedProject.remove();
+      return res.send(null);
+    } catch (error) {
+      res.status(500).json({
+        message: "На сервере произошла ошибка, попробуйте позже.",
+      });
+    }
+  });
 
 module.exports = router;
